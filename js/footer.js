cb74a5e9001d1c72ba2e7adf109e15a715809448
Sri Rakesh <sri.rakesh@runo.in>
2025-05-19 15:13:29 +0530

    new

---
+++ js/footer.js
@@ -0,0 +1,123 @@
+document.write(`
+<div class="container">
+            <div class="row">
+                <div class="col-md-offset-1 col-md-10 col-md-offset-1">
+                    <div class="col-md-3">
+                        <div class="footer-logo">
+                            <img src="../images/Runo-logo.png" alt="Runo Logo" width="80" />
+                        </div>
+                        <ul class="social-icons">
+                            <li>
+                                <a href="https://www.facebook.com/runo.in" target="_blank"><img src="../icons/facebook-icon.svg" width="25"
+                                        height="25" alt="Facebook">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.instagram.com/runo.in/" target="_blank"><img src="../icons/instagram-icon.svg" width="25"
+                                        height="25" alt="Instagram"></a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/company/runo-crm" target="_blank"><img src="../icons/linkedin-ic.svg" width="25"
+                                        height="25" alt="Linkedin">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.youtube.com/channel/UCff-NGVdfGJgMLtyZlzVGIA" target="_blank"><img
+                                        src="../icons/youtube-icon.svg" width="30" height="28" alt="Youtube"></a>
+                            </li>
+                        </ul>
+                        <div class="try-text">Let's try out!</div>
+                        <div>
+                            <a href="https://apps.apple.com/in/app/runo-crm-for-calling-process/id1528004506" class="download-link"
+                                target="_blank"><img src="../icons/app_store.svg" height="16" alt="App Store" /> App Store</a>
+                        </div>
+                        <div>
+                            <a href="https://play.google.com/store/apps/details?id=in.runo.call_crm&utm_source=website" target="_blank"
+                                class="download-link"><img src="../icons/play_store.svg" height="16" alt="Play Store" /> Play Store</a>
+                        </div>
+                        <ul class="footer-list">
+                            <li><a href="mailto:care@runo.in" rel="nofollow">care@runo.in</a></li>
+                            <li><a href="tel:+91 8179880074" rel="nofollow">+91 8179880074</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="footer-box">
+                            <h4>Company</h4>
+                            <ul class="footer-list">
+                                <li><a href="/">Home</a></li>
+                                <li><a href="../call-management-crm/">Call Management CRM</a></li>
+                                <li><a href="../pos-crm/">POS CRM</a></li>
+                                <li><a href="../field-crm/">Field CRM</a></li>
+                                <li><a href="../partners/">Partners</a></li>
+                                <li><a href="../contact-us/">Contact Us</a></li>
+                                <li><a href="https://blog.runo.in">Blog</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="footer-box">
+                            <h4>Policies</h4>
+                            <ul class="footer-list">
+                                <li><a href="../privacy-policy/">Privacy Policy</a></li>
+                                <li><a href="../terms-and-conditions/">Terms and Conditions</a></li>
+                                <li><a href="../cancellation-and-refund-policy/">Usage Policy</a></li>
+                                <li><a href="../data-security/">Data Security</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="badges-block">
+                            <div class="g2-badge">
+                                <a class="g2-link" href="https://www.g2.com/products/runo-call-management-crm/reviews" target="_blank">
+                                    <div class="rating-block">
+                                        <div class="rating-holder">
+                                            <div class="star-rating" data-rating-value="4.5">
+                                                <rating-item>1</rating-item>
+                                                <rating-item>2</rating-item>
+                                                <rating-item>3</rating-item>
+                                                <rating-item>4</rating-item>
+                                                <rating-item>5</rating-item>
+                                            </div>
+                                        </div>
+                                        <span class="rating-hint">Review us on</span>
+                                    </div>
+                                    <div class="vertical-line"></div>
+                                    <img src="../icons/g2-logo.svg" width="32" height="32"/>
+                                </a>
+                            </div>
+                            <div class="capterra-badge">
+                                <a class="capterra-link" href="https://www.capterra.com/p/214472/Call-Management-CRM/" target="_blank">
+                                    <div class="rating-block">
+                                        <div class="rating-holder">
+                                            <div class="star-rating-blue" data-rating-value="5">
+                                                <rating-item>1</rating-item>
+                                                <rating-item>2</rating-item>
+                                                <rating-item>3</rating-item>
+                                                <rating-item>4</rating-item>
+                                                <rating-item>5</rating-item>
+                                            </div>
+                                        </div>
+                                        <span class="rating-hint">Review us on</span>
+                                    </div>
+                                    <div class="vertical-line"></div>
+                                    <img src="../icons/capterra-logo.svg" width="36" height="36"/>
+                                </a>
+                            </div>
+                            <div class="crozdesk-badge">
+                                <a href="https://crozdesk.com/customer-service-crm/crm-software/runo-call-management-crm" target="_blank"
+                                    title="Runo Call Management - software ratings and reviews on Crozdesk"><img
+                                        src="//embed.crozdesk.com/badges/v2/rating/runo-call-management-crm" width="180" height="95"
+                                        alt="Runo Call Management reviews" /></a>
+                            </div>
+                            <div class="crozdesk-award">
+                                <a href="https://crozdesk.com/software/runo-call-management-crm" target="_blank"
+                                    title="Runo Call Management - software ratings and reviews on Crozdesk"><img
+                                        src="https://crozdesk.com/top_badges/crozdesk-quality-choice-badge.png" style="display:block;margin:auto;"
+                                        height="95" width="95" /></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+`);
---
+++ js/main.js
@@ -0,0 +1,227 @@
+jQuery(function ($) {
+  $("#owl-clients").owlCarousel({
+    center: false,
+    items: 6,
+    loop: false,
+    margin: 0,
+    responsiveClass: true,
+    nav: false,
+    mouseDrag: true,
+    touchDrag: true,
+    smartSpeed: 500,
+    autoplay: true,
+    autoplayTimeout: 8000,
+    autoplaySpeed: 900,
+    navText: [
+      "<img src='../icons/previous-arrow.svg' alt='Previous' width='12' /> Previous",
+      " Next <img src='../icons/next-arrow.svg' alt='Next' width='12' />",
+    ],
+    responsive: {
+      0: {
+        items: 2,
+        nav: false,
+        dots: false,
+        stagePadding: 0,
+        loop: true,
+        autoplay: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+        loop: true,
+        autoplay: false,
+      },
+      1000: {
+        nav: false,
+        dots: false,
+        loop: true,
+        items: 6,
+      },
+    },
+  });
+
+  $("#owl-reviews").owlCarousel({
+    center: false,
+    items: 1,
+    loop: false,
+    margin: 0,
+    responsiveClass: true,
+    nav: false,
+    mouseDrag: true,
+    touchDrag: true,
+    smartSpeed: 800,
+    autoplay: false,
+    autoplayTimeout: 8000,
+    autoplaySpeed: 900,
+    navText: [
+      "<img src='../icons/previous-arrow.svg' alt='Previous' width='12' /> Previous",
+      " Next <img src='../icons/next-arrow.svg' alt='Next' width='12' />",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+        dots: false,
+        stagePadding: 0,
+        loop: true,
+        center: false,
+        autoplay: false,
+      },
+      600: {
+        items: 1,
+        nav: false,
+        loop: true,
+        autoplay: false,
+      },
+      1000: {
+        nav: true,
+        dots: false,
+        loop: true,
+        items: 1,
+      },
+    },
+  });
+
+  window.onscroll = function () {
+    myFunction();
+  };
+  var header = document.getElementById("header");
+  var sticky = header.offsetTop;
+
+  function myFunction() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+      $("#hambr").attr("src", "../icons/hamburger.svg");
+    } else {
+      header.classList.remove("sticky");
+      $("#hambr").attr("src", "../icons/hamburger.svg");
+    }
+  }
+
+  $(".hamburger").click(function () {
+    $(".primary-nav").animate({
+      width: "toggle",
+    });
+    var src = $(".hamburger img").attr("src");
+    var newsrc =
+      src == "../icons/close.svg"
+        ? "../icons/hamburger.svg"
+        : "../icons/close.svg";
+    $(".hamburger img").attr("src", newsrc);
+    $("#body").toggleClass("hidden-body");
+  });
+
+  $(document).ready(function () {
+    function alignModal() {
+      var modalDialog = $(this).find(".modal-dialog");
+      /* Applying the top margin on modal dialog to align it vertically center */
+      modalDialog.css(
+        "margin-top",
+        Math.max(0, ($(window).height() - modalDialog.height()) / 2)
+      );
+    }
+    // Align modal when it is displayed
+    $(".modal").on("shown.bs.modal", alignModal);
+
+    // Align modal when user resize the window
+    $(window).on("resize", function () {
+      $(".modal:visible").each(alignModal);
+    });
+  });
+});
+
+function trackingListener() {
+  var capterra_vkey = "116a8e25097e6517cb232d28645832f0",
+    capterra_vid = "2154483",
+    ct = document.createElement("img");
+  ct.src =
+    "https://ct.capterra.com/capterra_tracker.gif?vid=" +
+    capterra_vid +
+    "&vkey=" +
+    capterra_vkey;
+  document.body.appendChild(ct);
+}
+
+function gtag_report_conversion(url) {
+  var callback = function () {
+    if (typeof url != "undefined") {
+      window.location = url;
+    }
+  };
+  gtag("event", "conversion", {
+    "send_to": "AW-613564663/0oOOCKS1rYQYEPeByaQC",
+    "event_callback": callback,
+  });
+  return false;
+}
+
+function showThankYou() {
+  jQuery(function ($) {
+    if ($("#thankYouModal").length == 0) {
+      $("body").append(
+        '<div class="modal" id="thankYouModal" tabindex="-1" role="dialog">' +
+          '<div class="modal-dialog modal-dialog-centered">' +
+          '<div class="modal-content" style="text-align: center;">' +
+          '<div class="modal-body">' +
+          '<svg xmlns="http://www.w3.org/2000/svg" class="text-success" width="75" height="75" fill="green" class="bi bi-check-circle-fill"' +
+          'viewBox="0 0 16 16">' +
+          "<path " +
+          'd="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />' +
+          "</svg>" +
+          '<button type="button" class="close close-md" data-dismiss="modal" aria-label="Close">' +
+          '<span aria-hidden="true">&times;</span>' +
+          "</button>" +
+          "</br>" +
+          "<h1>Thank You !</h1>" +
+          "<p>Our team will reach out to you soon.</p>" +
+          "</div>" +
+          "</div>" +
+          "</div>" +
+          "</div>"
+      );
+    }
+    $("#thankYouModal").modal("show");
+  });
+}
+
+function submitForm(formId, formData, formToken) {
+  jQuery(function ($) {
+    $(`#${formId}-btn`).prop("disabled", true);
+    const params = new URLSearchParams(window.location.search);
+    const utmSource = params.get("utm_source");
+    const utmCampaign = params.get("utm_campaign");
+    // const notes = `This lead has reached out to our website using utm_source as ${
+    //   utmSource || "N/A"
+    // } and utm_campaign as ${utmCampaign || "N/A"}`;
+    //formData["your_notes"] = notes;
+    formData["custom_source"] = "Website Enquiry- IB";
+    formData["custom_status"] = "Api Allocation";
+    if (utmSource) formData["custom_utm source"] = utmSource;
+    if (utmCampaign) formData["custom_utm campaign"] = utmCampaign;
+    $.ajax({
+      type: "POST",
+      url: `https://api-call-crm.runo.in/integration/webhook/wb/5d70a2816082af4daf1e377e/${formToken}`,
+      data: JSON.stringify(formData),
+      success: function () {},
+      headers: {
+        "Content-Type": "application/json",
+      },
+      contentType: "application/json",
+    })
+      .done(function (data) {
+        console.log(data);
+        $(`#${formId}`)[0].reset();
+        $(`#${formId}-btn`).prop("disabled", false);
+        $("#modal").modal("hide");
+        showThankYou();
+        trackingListener();
+        //Disabled as it's not working
+        //gtag_report_conversion();
+      })
+      .fail(function (a, b) {
+        console.log(a, b);
+        $(`#${formId}`)[0].reset();
+        $(`#${formId}-btn`).prop("disabled", false);
+      });
+  });
+}
